9be6aef8bd06de12368f0369a7650d42707003b3
Pavel Savinov <pavel.savinov@liferay.com>
2017-05-10 10:12:33 +0200

    LPS-72482 system portlet to add categoryid as a public render parameters in case when there is no p_p_id passed

---
+++ modules/apps/web-experience/layout/layout-type-controller/layout-type-controller-categorized/src/main/java/com/liferay/layout/type/controller/categorized/internal/portlet/CategorizedLayoutTypeControllerPortlet.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.type.controller.categorized.internal.portlet;
+
+import com.liferay.layout.type.controller.categorized.constants.CategorizedLayoutTypeControllerPortletKeys;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
+
+import javax.portlet.Portlet;
+
+import org.osgi.service.component.annotations.Component;
+
+/**
+ * @author Pavel Savinov
+ */
+@Component(
+	immediate = true,
+	property = {
+		"com.liferay.portlet.add-default-resource=true",
+		"com.liferay.portlet.private-request-attributes=false",
+		"com.liferay.portlet.private-session-attributes=false",
+		"com.liferay.portlet.render-weight=50",
+		"com.liferay.portlet.system=true",
+		"com.liferay.portlet.use-default-template=true",
+		"javax.portlet.display-name=Categorized Layout Type Controller",
+		"javax.portlet.init-param.template-path=/",
+		"javax.portlet.name=" + CategorizedLayoutTypeControllerPortletKeys.LAYOUT_TYPE_CATEGORIZED_PORTLET,
+		"javax.portlet.resource-bundle=content.Language",
+		"javax.portlet.security-role-ref=guest,power-user,user",
+		"javax.portlet.supported-public-render-parameter=categoryId",
+		"javax.portlet.supports.mime-type=text/html"
+	},
+	service = Portlet.class
+)
+public class CategorizedLayoutTypeControllerPortlet extends MVCPortlet {
+}
