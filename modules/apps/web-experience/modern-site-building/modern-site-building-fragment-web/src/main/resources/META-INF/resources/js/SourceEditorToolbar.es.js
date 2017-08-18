8b63de02835525ccc742d28bedf961103a406a33
Pablo Molina <pablo.molina@liferay.com>
2017-08-18 11:38:59 +0200

    LPS-73859 Added SourceEditorToolbar JS component

---
+++ modules/apps/web-experience/modern-site-building/modern-site-building-fragment-web/src/main/resources/META-INF/resources/js/SourceEditorToolbar.es.js
@@ -0,0 +1,83 @@
+import {isBoolean} from 'metal';
+import Component from 'metal-component';
+import Soy from 'metal-soy';
+import templates from './SourceEditorToolbar.soy';
+import {isOneOf} from './validators.es';
+import SelectAllToolbarItem from './toolbar-items/SelectAllToolbarItem.es';
+
+/**
+ * Toolbar item mapping used for creating functionality instances
+ * @type {Object}
+ */
+const ITEM_MAP = {
+	selectAll: SelectAllToolbarItem,
+};
+
+/**
+ * Component that adds functionality to an ace editor
+ * with a toolbar. Each item implements certain actions.
+ */
+class SourceEditorToolbar extends Component {
+	/** @inheritdoc */
+	rendered() {
+		if (this.showToolbar && this.aceEditor) {
+			this.items.forEach((itemName) => {
+				const ItemComponent = ITEM_MAP[itemName];
+				const itemWrapper = this.refs[itemName];
+
+				new ItemComponent({
+					aceEditor: this.aceEditor,
+				}, itemWrapper);
+			});
+		}
+	}
+
+	/**
+	 * Toggles the editor toolbar
+	 */
+	_toggleToolbar() {
+		this.showToolbar = !this.showToolbar;
+	}
+}
+
+SourceEditorToolbar.STATE = {
+	/**
+	 * Syntax used for the editor.
+	 * It will be shown inside the toolbar as toogle button.
+	 * @type {string}
+	 */
+	syntax: {
+		validator: isOneOf(['html', 'css', 'javascript']),
+	},
+
+	/**
+	 * Wherever or not show the editor toolbar
+	 * @type {boolean}
+	 */
+	showToolbar: {
+		value: true,
+		validator: isBoolean,
+	},
+
+	/**
+	 * Internal property used for storing Ace editor.
+	 * @type {Object}
+	 */
+	aceEditor: {
+		value: null,
+	},
+
+	/**
+	 * Array of items that should be rendered inside the toolbar,
+	 * each item will be placed inside the toolbar, and receive
+	 * the existing editor as parameter.
+	 * @type {Array<string>}
+	 */
+	items: {
+		value: [],
+	},
+};
+
+Soy.register(SourceEditorToolbar, templates);
+
+export default SourceEditorToolbar;
