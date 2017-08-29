f0a91c60e39dfddfc7d8bdd49c27a9d17dc1bfdf
Pavel Savinov <pavel.savinov@liferay.com>
2017-08-29 15:29:40 +0200

    LPS-74411 Create layout to render asset display template on it

--- modules/apps/web-experience/asset/asset-display-template-service/src/main/java/com/liferay/asset/display/template/service/impl/AssetDisplayTemplateLocalServiceImpl.java
+++ modules/apps/web-experience/asset/asset-display-template-service/src/main/java/com/liferay/asset/display/template/service/impl/AssetDisplayTemplateLocalServiceImpl.java
@@ -20,13 +20,17 @@ import com.liferay.asset.display.template.util.comparator.AssetDisplayTemplateCr
 import com.liferay.dynamic.data.mapping.kernel.DDMTemplate;
 import com.liferay.dynamic.data.mapping.kernel.DDMTemplateManager;
 import com.liferay.dynamic.data.mapping.service.DDMTemplateLocalService;
+import com.liferay.layout.type.controller.asset.display.constants.AssetDisplayLayoutTypeControllerConstants;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.Layout;
 import com.liferay.portal.kernel.model.ResourceConstants;
 import com.liferay.portal.kernel.model.User;
 import com.liferay.portal.kernel.service.ServiceContext;
 import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.OrderByComparator;
 import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.StringPool;
+import com.liferay.portal.kernel.util.UnicodeProperties;
 import com.liferay.portal.spring.extender.service.ServiceReference;
 import com.liferay.portlet.display.template.PortletDisplayTemplate;
 
@@ -90,6 +94,25 @@ public class AssetDisplayTemplateLocalServiceImpl
 
 		assetDisplayTemplatePersistence.update(assetDisplayTemplate);
 
+		// Layout
+
+		Layout layout = layoutLocalService.addLayout(
+			userId, groupId, false, 0, name, name, StringPool.BLANK,
+			AssetDisplayLayoutTypeControllerConstants.LAYOUT_TYPE_ASSET_DISPLAY,
+			true, null, serviceContext);
+
+		assetDisplayTemplate.setPlid(layout.getPlid());
+
+		UnicodeProperties typeSettingsProperties =
+			layout.getTypeSettingsProperties();
+
+		typeSettingsProperties.setProperty(
+			"assetDisplayTemplateId", String.valueOf(assetDisplayTemplateId));
+
+		layoutLocalService.updateLayout(
+			groupId, layout.isPrivateLayout(), layout.getLayoutId(),
+			layout.getTypeSettings());
+
 		// Resources
 
 		resourceLocalService.addModelResources(
@@ -111,6 +134,10 @@ public class AssetDisplayTemplateLocalServiceImpl
 
 		_ddmTemplateLocalService.deleteDDMTemplate(
 			assetDisplayTemplate.getDDMTemplateId());
+
+		// Layout
+
+		layoutLocalService.deleteLayout(assetDisplayTemplate.getPlid());
 
 		// Resources
 
