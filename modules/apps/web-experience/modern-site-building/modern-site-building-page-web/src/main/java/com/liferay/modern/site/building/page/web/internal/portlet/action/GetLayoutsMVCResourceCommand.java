303f5940951d640057d80b1f249993234c11084e
Pavel Savinov <pavel.savinov@liferay.com>
2017-09-01 09:59:50 +0200

    LPS-74461 Ability to get layouts array asynchronously from pages list component

---
+++ modules/apps/web-experience/modern-site-building/modern-site-building-page-web/src/main/java/com/liferay/modern/site/building/page/web/internal/portlet/action/GetLayoutsMVCResourceCommand.java
@@ -0,0 +1,146 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.modern.site.building.page.web.internal.portlet.action;
+
+import com.liferay.modern.site.building.page.web.constants.PagesPortletKeys;
+import com.liferay.modern.site.building.page.web.internal.util.PagesPortletUtil;
+import com.liferay.portal.kernel.dao.orm.DynamicQuery;
+import com.liferay.portal.kernel.dao.orm.Property;
+import com.liferay.portal.kernel.dao.orm.PropertyFactoryUtil;
+import com.liferay.portal.kernel.dao.orm.QueryUtil;
+import com.liferay.portal.kernel.json.JSONArray;
+import com.liferay.portal.kernel.json.JSONFactoryUtil;
+import com.liferay.portal.kernel.json.JSONObject;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.portlet.JSONPortletResponseUtil;
+import com.liferay.portal.kernel.portlet.bridges.mvc.BaseMVCResourceCommand;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCResourceCommand;
+import com.liferay.portal.kernel.service.LayoutLocalService;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.OrderByComparator;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.WebKeys;
+
+import java.util.List;
+
+import javax.portlet.ResourceRequest;
+import javax.portlet.ResourceResponse;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Pavel Savinov
+ */
+@Component(
+	immediate = true,
+	property = {
+		"javax.portlet.name=" + PagesPortletKeys.PAGES,
+		"mvc.command.name=getLayouts"
+	},
+	service = MVCResourceCommand.class
+)
+public class GetLayoutsMVCResourceCommand extends BaseMVCResourceCommand {
+
+	@Override
+	protected void doServeResource(
+			ResourceRequest resourceRequest, ResourceResponse resourceResponse)
+		throws Exception {
+
+		JSONArray jsonArray = JSONFactoryUtil.createJSONArray();
+
+		long groupId = ParamUtil.getLong(resourceRequest, "groupId");
+		boolean loadParentLayouts = ParamUtil.getBoolean(
+			resourceRequest, "loadParentLayouts");
+		long parentLayoutId = ParamUtil.getLong(
+			resourceRequest, "parentLayoutId");
+		boolean privateLayout = ParamUtil.getBoolean(
+			resourceRequest, "privateLayout");
+		long selectedLayoutId = ParamUtil.getLong(
+			resourceRequest, "selectedLayoutId");
+
+		String orderByCol = ParamUtil.getString(resourceRequest, "orderByCol");
+		String orderByType = ParamUtil.getString(
+			resourceRequest, "orderByType");
+
+		OrderByComparator orderByComparator =
+			PagesPortletUtil.getLayoutOrderByComparator(
+				orderByCol, orderByType);
+
+		Layout parentLayout = _layoutLocalService.fetchLayout(
+			groupId, privateLayout, parentLayoutId);
+
+		if (loadParentLayouts && (parentLayout != null)) {
+			parentLayoutId = parentLayout.getParentLayoutId();
+
+			selectedLayoutId = parentLayout.getLayoutId();
+		}
+
+		DynamicQuery dynamicQuery = _layoutLocalService.dynamicQuery();
+
+		Property groupIdProperty = PropertyFactoryUtil.forName("groupId");
+		Property privateLayoutProperty = PropertyFactoryUtil.forName(
+			"privateLayout");
+		Property parentLayoutIdProperty = PropertyFactoryUtil.forName(
+			"parentLayoutId");
+
+		dynamicQuery.add(groupIdProperty.eq(groupId));
+		dynamicQuery.add(privateLayoutProperty.eq(privateLayout));
+		dynamicQuery.add(parentLayoutIdProperty.eq(parentLayoutId));
+
+		List<Layout> layouts = _layoutLocalService.dynamicQuery(
+			dynamicQuery, QueryUtil.ALL_POS, QueryUtil.ALL_POS,
+			orderByComparator);
+
+		HttpServletRequest request = _portal.getHttpServletRequest(
+			resourceRequest);
+
+		ThemeDisplay themeDisplay = (ThemeDisplay)request.getAttribute(
+			WebKeys.THEME_DISPLAY);
+
+		for (Layout layout : layouts) {
+			JSONObject jsonObject = JSONFactoryUtil.createJSONObject();
+
+			JSONObject actionsJSONObject =
+				PagesPortletUtil.getActionsJSONObject(layout, request);
+
+			if (actionsJSONObject.length() > 0) {
+				jsonObject.put("actions", actionsJSONObject);
+			}
+
+			jsonObject.put("active", layout.getLayoutId() == selectedLayoutId);
+			jsonObject.put("icon", "page");
+			jsonObject.put("layoutId", layout.getLayoutId());
+			jsonObject.put("parentLayoutId", layout.getParentLayoutId());
+			jsonObject.put("selected", jsonObject.getBoolean("active"));
+			jsonObject.put("title", layout.getName(themeDisplay.getLocale()));
+
+			jsonArray.put(jsonObject);
+		}
+
+		JSONPortletResponseUtil.writeJSON(
+			resourceRequest, resourceResponse, jsonArray);
+	}
+
+	@Reference
+	private LayoutLocalService _layoutLocalService;
+
+	@Reference
+	private Portal _portal;
+
+}
