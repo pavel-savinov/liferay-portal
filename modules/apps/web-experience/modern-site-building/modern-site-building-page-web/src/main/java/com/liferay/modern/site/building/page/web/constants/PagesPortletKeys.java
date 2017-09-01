dedf01fbf9007b3f370674b6d3e8f4fe565976ec
Pavel Savinov <pavel.savinov@liferay.com>
2017-09-01 09:19:44 +0200

    LPS-74461 Pages admin portlet base

---
+++ modules/apps/web-experience/modern-site-building/modern-site-building-page-web/src/main/java/com/liferay/modern/site/building/page/web/constants/PagesPortletKeys.java
@@ -0,0 +1,25 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.modern.site.building.page.web.constants;
+
+/**
+ * @author Pavel Savinov
+ */
+public class PagesPortletKeys {
+
+	public static final String PAGES =
+		"com_liferay_modern_site_building_page_web_portlet_PagesPortlet";
+
+}
