44d5c79cb495cf949d01a56d2e88f88c5a23d570
Pavel Savinov <pavel.savinov@liferay.com>
2017-09-01 09:58:08 +0200

    LPS-74461 Page list block shows block of pages in pages list

---
+++ modules/apps/web-experience/modern-site-building/modern-site-building-page-web/src/main/resources/META-INF/resources/js/PageListBlock.es.js
@@ -0,0 +1,84 @@
+import Component from 'metal-component';
+import {dom} from 'metal-dom';
+import {hasClass} from 'metal-dom';
+import {Config} from 'metal-state';
+import Soy from 'metal-soy';
+import 'metal-dropdown';
+import templates from './PageListBlock.soy';
+
+/**
+ * Component that represents one block of layouts for <PageList /> component.
+ */
+class PageListBlock extends Component {
+
+	/**
+	 * This is called when one of this block's nodes is clicked.
+	 * @param {!Event} event
+	 * @private
+	 */
+	_handleNodeClicked(event) {
+		let target = event.target;
+
+		if (hasClass(target, 'action-icon')) {
+			return;
+		}
+
+		const layoutId = event.delegateTarget.dataset.layoutid;
+		const parentLayoutId = event.delegateTarget.dataset.parentlayoutid;
+		const title = event.delegateTarget.dataset.title;
+
+		this._selectNode(layoutId, parentLayoutId, title);
+	}
+
+	/**
+	 * Selects specific node and emits associated event.
+	 *
+	 * @param {number} layoutId Node's layout ID
+	 * @param {number} parentLayoutId Node's parent layout ID
+	 * @param {string} title Node's title
+	 * @private
+	 */
+	_selectNode(layoutId, parentLayoutId, title) {
+		this.nodes.forEach(
+			(node) => {
+				if (node.layoutId !== layoutId) {
+					node.selected = false;
+				}				else {
+					node.active = true;
+					node.selected = true;
+				}
+			},
+			this
+		);
+
+		this.nodes = this.nodes;
+
+		if (parentLayoutId >= 0) {
+			this.emit(
+				'nodeSelected',
+				{
+					layoutId: layoutId,
+					loadParents: true,
+					parentLayoutId: parentLayoutId,
+					ref: this.getInitialConfig().ref,
+					title: title,
+				}
+			);
+		}
+	}
+
+}
+
+PageListBlock.STATE = {
+	/**
+	 * Current block nodes list
+	 * @instance
+	 * @memberof PageListBlock
+	 * @type {!Array}
+	 */
+	nodes: Config.array().required(),
+};
+
+Soy.register(PageListBlock, templates);
+
+export default PageListBlock;
