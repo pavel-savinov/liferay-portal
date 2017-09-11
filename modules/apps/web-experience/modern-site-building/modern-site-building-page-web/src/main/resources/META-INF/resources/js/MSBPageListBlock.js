5127b8edb4b827d7ecc83fc6420e824348a5a67b
Pablo Molina <pablo.molina@liferay.com>
2017-09-11 11:39:31 +0200

    LPS-74461 Simplified MSBPageListBlock

--- modules/apps/web-experience/modern-site-building/modern-site-building-page-web/src/main/resources/META-INF/resources/js/MSBPageListBlock.js
+++ modules/apps/web-experience/modern-site-building/modern-site-building-page-web/src/main/resources/META-INF/resources/js/MSBPageListBlock.js
@@ -1,6 +1,4 @@
 import Component from 'metal-component';
-import {dom} from 'metal-dom';
-import {hasClass} from 'metal-dom';
 import {Config} from 'metal-state';
 import Soy from 'metal-soy';
 import 'metal-dropdown';
@@ -12,62 +10,6 @@ import templates from './MSBPageListBlock.soy';
  * Component that represents one block of layouts for <MSBPageList /> component.
  */
 class MSBPageListBlock extends Component {
-	/**
-	 * This is called when one of this block's nodes is clicked.
-	 * @param {!Event} event
-	 * @protected
-	 */
-	_handleNodeClicked(event) {
-		const target = event.target;
-
-		if (hasClass(target, 'action-icon')) {
-			return;
-		}
-
-		const layoutId = event.delegateTarget.dataset.layoutid;
-		const parentLayoutId = event.delegateTarget.dataset.parentlayoutid;
-		const title = event.delegateTarget.dataset.title;
-
-		this._selectNode(layoutId, parentLayoutId, title);
-	}
-
-	/**
-	 * Selects specific node and emits associated event.
-	 *
-	 * @param {number} layoutId Node's layout ID
-	 * @param {number} parentLayoutId Node's parent layout ID
-	 * @param {string} title Node's title
-	 * @protected
-	 */
-	_selectNode(layoutId, parentLayoutId, title) {
-		this.nodes.forEach(
-			(node) => {
-				if (node.layoutId !== layoutId) {
-					node.selected = false;
-				}
-				else {
-					node.active = true;
-					node.selected = true;
-				}
-			},
-			this
-		);
-
-		this.nodes = this.nodes;
-
-		if (parentLayoutId >= 0) {
-			this.emit(
-				'nodeSelected',
-				{
-					layoutId: layoutId,
-					loadParents: true,
-					parentLayoutId: parentLayoutId,
-					ref: this.getInitialConfig().ref,
-					title: title,
-				}
-			);
-		}
-	}
 }
 
 /**
