e9353379865c636f699681e0aae70792bae511e9
pavel-savinov <pavel.savinov@liferay.com>
2018-05-08 10:31:14 +0200

    LPS-80145 Add layout edit mode body css class

---
+++ modules/apps/layout/layout-admin-web/src/main/java/com/liferay/layout/admin/web/internal/theme/contributor/LayoutEditModeTemplateContextContributor.java
@@ -0,0 +1,73 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.layout.admin.web.internal.theme.contributor;
+
+import com.liferay.layout.admin.web.internal.control.menu.LayoutEditModeProductNavigationControlMenuEntry;
+import com.liferay.petra.string.StringPool;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.log.Log;
+import com.liferay.portal.kernel.log.LogFactoryUtil;
+import com.liferay.portal.kernel.template.TemplateContextContributor;
+import com.liferay.portal.kernel.util.GetterUtil;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Pavel Savinov
+ */
+@Component(
+	immediate = true,
+	property = "type=" + TemplateContextContributor.TYPE_THEME,
+	service = TemplateContextContributor.class
+)
+public class LayoutEditModeTemplateContextContributor
+	implements TemplateContextContributor {
+
+	@Override
+	public void prepare(
+		Map<String, Object> contextObjects, HttpServletRequest request) {
+
+		try {
+			if (_layoutEditModeProductNavigationControlMenuEntry.isShow(
+					request)) {
+
+				StringBuilder sb = new StringBuilder(3);
+
+				sb.append(
+					GetterUtil.getString(contextObjects.get("bodyCssClass")));
+				sb.append(StringPool.SPACE);
+				sb.append("has-edit-mode-menu");
+
+				contextObjects.put("bodyCssClass", sb.toString());
+			}
+		}
+		catch (PortalException pe) {
+			_log.error(pe, pe);
+		}
+	}
+
+	private static final Log _log = LogFactoryUtil.getLog(
+		LayoutEditModeTemplateContextContributor.class);
+
+	@Reference
+	private LayoutEditModeProductNavigationControlMenuEntry
+		_layoutEditModeProductNavigationControlMenuEntry;
+
+}
