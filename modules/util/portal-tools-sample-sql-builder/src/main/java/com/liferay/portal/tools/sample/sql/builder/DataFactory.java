34af5ded258aea87a93cf575b7117dccc51b18be
Eudaldo Alonso <eudaldo.alonso@liferay.com>
2018-07-24 09:29:49 +0200

    LPS-83616 No needed exceptions

--- modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
+++ modules/util/portal-tools-sample-sql-builder/src/main/java/com/liferay/portal/tools/sample/sql/builder/DataFactory.java
@@ -1983,9 +1983,8 @@ public class DataFactory {
 	}
 
 	public FragmentEntryLinkModel newFragmentEntryLinkModel(
-			LayoutModel layoutModel, FragmentEntryModel fragmentEntryModel,
-			int position)
-		throws Exception {
+        LayoutModel layoutModel, FragmentEntryModel fragmentEntryModel,
+        int position) {
 
 		FragmentEntryLinkModel fragmentEntryLinkModel =
 			new FragmentEntryLinkModelImpl();
@@ -2012,9 +2011,8 @@ public class DataFactory {
 	}
 
 	public List<FragmentEntryLinkModel> newFragmentEntryLinkModels(
-			LayoutModel layoutModel,
-			Map<String, FragmentEntryModel> fragmentEntryModels)
-		throws Exception {
+        LayoutModel layoutModel,
+		Map<String, FragmentEntryModel> fragmentEntryModels) {
 
 		List<FragmentEntryLinkModel> fragmentEntryLinkModels =
 			new ArrayList<>();
