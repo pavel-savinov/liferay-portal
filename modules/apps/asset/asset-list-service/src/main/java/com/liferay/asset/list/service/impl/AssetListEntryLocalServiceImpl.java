98d96710a7b5cda85a3cfaaf14ab318c1a917080
Jürgen Kappler <jurgen.kappler@liferay.com>
2018-08-20 17:49:11 +0200

    LPS-84376 Implement basic methods to view, add, update and delete asset lists

--- modules/apps/asset/asset-list-service/src/main/java/com/liferay/asset/list/service/impl/AssetListEntryLocalServiceImpl.java
+++ modules/apps/asset/asset-list-service/src/main/java/com/liferay/asset/list/service/impl/AssetListEntryLocalServiceImpl.java
@@ -14,29 +14,155 @@
 
 package com.liferay.asset.list.service.impl;
 
+import com.liferay.asset.list.exception.NoSuchEntryException;
+import com.liferay.asset.list.model.AssetListEntry;
+import com.liferay.asset.list.model.AssetListEntryLocalization;
 import com.liferay.asset.list.service.base.AssetListEntryLocalServiceBaseImpl;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.SystemEventConstants;
+import com.liferay.portal.kernel.model.User;
+import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.systemevent.SystemEvent;
+import com.liferay.portal.kernel.util.Validator;
+
+import java.util.Date;
+import java.util.Map;
 
 /**
- * The implementation of the asset list entry local service.
- *
- * <p>
- * All custom service methods should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the {@link com.liferay.asset.list.service.AssetListEntryLocalService} interface.
- *
- * <p>
- * This is a local service. Methods of this service will not have security checks based on the propagated JAAS credentials because this service can only be accessed from within the same VM.
- * </p>
- *
- * @author Brian Wing Shun Chan
- * @see AssetListEntryLocalServiceBaseImpl
- * @see com.liferay.asset.list.service.AssetListEntryLocalServiceUtil
+ * @author Jürgen Kappler
  */
 public class AssetListEntryLocalServiceImpl
 	extends AssetListEntryLocalServiceBaseImpl {
 
-	/**
-	 * NOTE FOR DEVELOPERS:
-	 *
-	 * Never reference this class directly. Always use {@link com.liferay.asset.list.service.AssetListEntryLocalServiceUtil} to access the asset list entry local service.
-	 */
+	@Override
+	public AssetListEntry addAssetListEntry(
+			long userId, long groupId, Map<String, String> titleMap,
+			Map<String, String> descriptionMap, int type,
+			ServiceContext serviceContext)
+		throws PortalException {
+
+		User user = userLocalService.getUser(userId);
+
+		long assetListEntryId = counterLocalService.increment();
+
+		AssetListEntry assetListEntry = assetListEntryPersistence.create(
+			assetListEntryId);
+
+		assetListEntry.setUuid(serviceContext.getUuid());
+		assetListEntry.setGroupId(groupId);
+		assetListEntry.setCompanyId(user.getCompanyId());
+		assetListEntry.setUserId(user.getUserId());
+		assetListEntry.setUserName(user.getFullName());
+		assetListEntry.setCreateDate(serviceContext.getCreateDate(new Date()));
+		assetListEntry.setModifiedDate(
+			serviceContext.getModifiedDate(new Date()));
+		assetListEntry.setType(type);
+
+		Group group = groupLocalService.getGroup(groupId);
+
+		_updateAssetListEntryLocalizations(
+			assetListEntryId, group.getCompanyId(), groupId, titleMap,
+			descriptionMap);
+
+		assetListEntryPersistence.update(assetListEntry);
+
+		return assetListEntry;
+	}
+
+	@Override
+	public AssetListEntry deleteAssetListEntry(AssetListEntry assetListEntry)
+		throws PortalException {
+
+		assetListEntryLocalService.deleteAssetListEntry(
+			assetListEntry.getAssetListEntryId());
+
+		return assetListEntry;
+	}
+
+	@Override
+	@SystemEvent(type = SystemEventConstants.TYPE_DELETE)
+	public AssetListEntry deleteAssetListEntry(long assetListEntryId)
+		throws NoSuchEntryException {
+
+		AssetListEntry assetListEntry =
+			assetListEntryPersistence.fetchByPrimaryKey(assetListEntryId);
+
+		if (assetListEntry == null) {
+			throw new NoSuchEntryException();
+		}
+
+		assetListEntryLocalizationPersistence.removeByAssetListEntryId(
+			assetListEntryId);
+
+		return assetListEntryPersistence.remove(assetListEntryId);
+	}
+
+	@Override
+	public AssetListEntry updateAssetListEntry(
+			long assetListEntryId, Map<String, String> titleMap,
+			Map<String, String> descriptionMap)
+		throws PortalException {
+
+		AssetListEntry assetListEntry =
+			assetListEntryPersistence.findByPrimaryKey(assetListEntryId);
+
+		assetListEntry.setModifiedDate(new Date());
+
+		_updateAssetListEntryLocalizations(
+			assetListEntryId, assetListEntry.getCompanyId(),
+			assetListEntry.getGroupId(), titleMap, descriptionMap);
+
+		assetListEntryPersistence.update(assetListEntry);
+
+		return assetListEntry;
+	}
+
+	private void _updateAssetListEntryLocalizations(
+		long assetListEntryId, long companyId, long groupId,
+		Map<String, String> titleMap, Map<String, String> descriptionMap) {
+
+		for (Map.Entry<String, String> entry : titleMap.entrySet()) {
+			String title = entry.getValue();
+
+			if (Validator.isNull(title)) {
+				continue;
+			}
+
+			String description = descriptionMap.get(entry.getKey());
+
+			AssetListEntryLocalization existingAssetListEntryLocalization =
+				assetListEntryLocalizationPersistence.
+					fetchByAssetListEntryId_LanguageId(
+						assetListEntryId, entry.getKey());
+
+			if (existingAssetListEntryLocalization != null) {
+				existingAssetListEntryLocalization.setTitle(title);
+				existingAssetListEntryLocalization.setDescription(description);
+
+				assetListEntryLocalizationPersistence.update(
+					existingAssetListEntryLocalization);
+
+				continue;
+			}
+
+			long assetListEntryLocalizationId = counterLocalService.increment(
+				AssetListEntryLocalization.class.getName());
+
+			AssetListEntryLocalization assetListEntryLocalization =
+				assetListEntryLocalizationPersistence.create(
+					assetListEntryLocalizationId);
+
+			assetListEntryLocalization.setCompanyId(companyId);
+			assetListEntryLocalization.setAssetListEntryId(assetListEntryId);
+			assetListEntryLocalization.setGroupId(groupId);
+			assetListEntryLocalization.setTitle(title);
+			assetListEntryLocalization.setDescription(description);
+			assetListEntryLocalization.setLanguageId(entry.getKey());
+
+			assetListEntryLocalizationPersistence.update(
+				assetListEntryLocalization);
+		}
+	}
 
 }
