79d6c73cf5ddbaa9ef92c09ff7383d7a36d11095
Jürgen Kappler <jurgen.kappler@liferay.com>
2019-01-31 15:57:48 +0100

    LPS-89973 Add to upgrade process

--- modules/apps/fragment/fragment-service/src/main/resources/com/liferay/fragment/internal/upgrade/v2_1_0/dependencies/update.sql
+++ modules/apps/fragment/fragment-service/src/main/resources/com/liferay/fragment/internal/upgrade/v2_1_0/dependencies/update.sql
@@ -1,5 +1,7 @@
 alter table FragmentEntry add type_ INTEGER;
+alter table FragmentEntryLink add type_ INTEGER;
 
 COMMIT_TRANSACTION;
 
 update FragmentEntry set type_ = 0;
+update FragmentEntryLink set type_ = 0;
