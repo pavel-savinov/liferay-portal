5400674b7aa853b354c7fd0e67a136127721d98a
Chema Balsas <jose.balsas@liferay.com>
2019-02-04 14:52:46 -0800

    LPS-89705 Runs build-taglibs (auto-generated)

--- util-taglib/src/com/liferay/taglib/aui/base/BasePanelTag.java
+++ util-taglib/src/com/liferay/taglib/aui/base/BasePanelTag.java
@@ -24,7 +24,7 @@ import javax.servlet.jsp.JspException;
  * @author Julio Camarero
  * @generated
  */
-public abstract class BaseFieldsetTag extends com.liferay.taglib.util.IncludeTag {
+public abstract class BasePanelTag extends com.liferay.taglib.util.IncludeTag {
 
 	@Override
 	public int doStartTag() throws JspException {
@@ -41,18 +41,6 @@ public abstract class BaseFieldsetTag extends com.liferay.taglib.util.IncludeTag
 		return _collapsible;
 	}
 
-	public boolean getColumn() {
-		return _column;
-	}
-
-	public java.lang.String getCssClass() {
-		return _cssClass;
-	}
-
-	public java.lang.String getHelpMessage() {
-		return _helpMessage;
-	}
-
 	public java.lang.String getId() {
 		return _id;
 	}
@@ -65,28 +53,12 @@ public abstract class BaseFieldsetTag extends com.liferay.taglib.util.IncludeTag
 		return _localizeLabel;
 	}
 
-	public java.lang.String getMarkupView() {
-		return _markupView;
-	}
-
 	public void setCollapsed(boolean collapsed) {
 		_collapsed = collapsed;
 	}
 
 	public void setCollapsible(boolean collapsible) {
 		_collapsible = collapsible;
-	}
-
-	public void setColumn(boolean column) {
-		_column = column;
-	}
-
-	public void setCssClass(java.lang.String cssClass) {
-		_cssClass = cssClass;
-	}
-
-	public void setHelpMessage(java.lang.String helpMessage) {
-		_helpMessage = helpMessage;
 	}
 
 	public void setId(java.lang.String id) {
@@ -101,23 +73,15 @@ public abstract class BaseFieldsetTag extends com.liferay.taglib.util.IncludeTag
 		_localizeLabel = localizeLabel;
 	}
 
-	public void setMarkupView(java.lang.String markupView) {
-		_markupView = markupView;
-	}
-
 	@Override
 	protected void cleanUp() {
 		super.cleanUp();
 
 		_collapsed = false;
 		_collapsible = false;
-		_column = false;
-		_cssClass = null;
-		_helpMessage = null;
 		_id = null;
 		_label = null;
 		_localizeLabel = true;
-		_markupView = null;
 	}
 
 	@Override
@@ -134,31 +98,23 @@ public abstract class BaseFieldsetTag extends com.liferay.taglib.util.IncludeTag
 	protected void setAttributes(HttpServletRequest request) {
 		setNamespacedAttribute(request, "collapsed", _collapsed);
 		setNamespacedAttribute(request, "collapsible", _collapsible);
-		setNamespacedAttribute(request, "column", _column);
-		setNamespacedAttribute(request, "cssClass", _cssClass);
-		setNamespacedAttribute(request, "helpMessage", _helpMessage);
 		setNamespacedAttribute(request, "id", _id);
 		setNamespacedAttribute(request, "label", _label);
 		setNamespacedAttribute(request, "localizeLabel", _localizeLabel);
-		setNamespacedAttribute(request, "markupView", _markupView);
 	}
 
-	protected static final String _ATTRIBUTE_NAMESPACE = "aui:fieldset:";
+	protected static final String _ATTRIBUTE_NAMESPACE = "aui:panel:";
 
 	private static final String _END_PAGE =
-		"/html/taglib/aui/fieldset/end.jsp";
+		"/html/taglib/aui/panel/end.jsp";
 
 	private static final String _START_PAGE =
-		"/html/taglib/aui/fieldset/start.jsp";
+		"/html/taglib/aui/panel/start.jsp";
 
 	private boolean _collapsed = false;
 	private boolean _collapsible = false;
-	private boolean _column = false;
-	private java.lang.String _cssClass = null;
-	private java.lang.String _helpMessage = null;
 	private java.lang.String _id = null;
 	private java.lang.String _label = null;
 	private boolean _localizeLabel = true;
-	private java.lang.String _markupView = null;
 
 }
