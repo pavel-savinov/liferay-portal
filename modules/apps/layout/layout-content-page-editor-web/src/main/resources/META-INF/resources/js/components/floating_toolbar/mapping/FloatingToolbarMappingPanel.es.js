e696488719a4aa55aed09041fb9a7e3e715e4c9b
Sandro Chinea <sandrochinea@gmail.com>
2019-03-12 18:20:12 +0100

    LPS-89680 SF

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/js/components/floating_toolbar/mapping/FloatingToolbarMappingPanel.es.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/js/components/floating_toolbar/mapping/FloatingToolbarMappingPanel.es.js
@@ -4,7 +4,7 @@ import Soy, {Config} from 'metal-soy';
 
 import './FloatingToolbarMappingPanelDelegateTemplate.soy';
 import {COMPATIBLE_TYPES} from '../../../utils/constants';
-import {decodeId, encodeAssetId} from '../../../utils/FragmentsEditorIdUtils.es';
+import {encodeAssetId} from '../../../utils/FragmentsEditorIdUtils.es';
 import getConnectedComponent from '../../../store/ConnectedComponent.es';
 import {setIn} from '../../../utils/FragmentsEditorUpdateUtils.es';
 import templates from './FloatingToolbarMappingPanel.soy';
@@ -66,17 +66,17 @@ class FloatingToolbarMappingPanel extends PortletBase {
 		);
 
 		if (
-      nextState.mappingFieldsURL &&
-      nextState.selectedMappingTypes &&
-      nextState.selectedMappingTypes.type
-    ) {
+			nextState.mappingFieldsURL &&
+			nextState.selectedMappingTypes &&
+			nextState.selectedMappingTypes.type
+		) {
 			nextState = setIn(
 				nextState,
 				['_sourceTypes'],
 				FloatingToolbarMappingPanel._getSourceTypes(
-          nextState.selectedMappingTypes.subtype ?
-            nextState.selectedMappingTypes.subtype.label :
-  					nextState.selectedMappingTypes.type.label
+					nextState.selectedMappingTypes.subtype ?
+						nextState.selectedMappingTypes.subtype.label :
+						nextState.selectedMappingTypes.type.label
 				)
 			);
 		}
@@ -159,14 +159,14 @@ class FloatingToolbarMappingPanel extends PortletBase {
 	 * @review
 	 */
 	_handleAssetBrowserLinkClick(event) {
-		const { assetBrowserUrl, assetBrowserWindowTitle } = event.delegateTarget.dataset;
+		const {assetBrowserUrl, assetBrowserWindowTitle} = event.delegateTarget.dataset;
 
 		openAssetBrowser(
 			assetBrowserUrl,
 			assetBrowserWindowTitle,
 			this.portletNamespace,
 			selectedAssetEntry => {
-				this._selectAssetEntry(selectedAssetEntry);
+				this._selectAssetEn tryselectedAssetEntry);
 
 				this.store.dispatchAction(
 					ADD_MAPPED_ASSET_ENTRY,
@@ -190,7 +190,7 @@ class FloatingToolbarMappingPanel extends PortletBase {
 	_handleAssetEntryLinkClick(event) {
 		const data = event.delegateTarget.dataset;
 
-		this._selectAssetEntry(
+		this._selectAssetEn try
 			{
 				classNameId: data.classNameId,
 				classPK: data.classPk
@@ -260,13 +260,13 @@ class FloatingToolbarMappingPanel extends PortletBase {
 		this._clearFields();
 
 		if (this._selectedSourceTypeId === SOURCE_TYPE_IDS.structure) {
-      const data = {
-        classNameId: this.selectedMappingTypes.type.id
-      };
+			const data = {
+				classNameId: this.selectedMappingTypes.type.id
+			};
 
-      if (this.selectedMappingTypes.subtype) {
-        data.classTypeId = this.selectedMappingTypes.subtype.id;
-      }
+			if (this.selectedMappingTypes.subtype) {
+				data.classTypeId = this.selectedMappingTypes.subtype.id;
+			}
 
 			promise = this.fetch(this.mappingFieldsURL, data);
 		}
@@ -310,7 +310,7 @@ class FloatingToolbarMappingPanel extends PortletBase {
 	 * @private
 	 * @review
 	 */
-	_selectAssetEntry(assetEntry) {
+	_selectAssetEn tryassetEntry) {
 		this._updateEditableValues('classNameId', assetEntry.classNameId);
 		this._updateEditableValues('classPK', assetEntry.classPK);
 		this._updateEditableValues('fieldId', '');
