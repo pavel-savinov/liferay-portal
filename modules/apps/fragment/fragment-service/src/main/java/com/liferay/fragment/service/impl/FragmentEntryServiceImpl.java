e8fcf10bd33f00f7d0f219e9b06530cfeb1bca9f
Pavel Savinov <pavel.savinov@liferay.com>
2019-06-05 11:36:48 +0200

    LPS-96435 Remote service to render a fragment preview which was not saved yet

--- modules/apps/fragment/fragment-service/src/main/java/com/liferay/fragment/service/impl/FragmentEntryServiceImpl.java
+++ modules/apps/fragment/fragment-service/src/main/java/com/liferay/fragment/service/impl/FragmentEntryServiceImpl.java
@@ -16,21 +16,46 @@ package com.liferay.fragment.service.impl;
 
 import com.liferay.fragment.constants.FragmentActionKeys;
 import com.liferay.fragment.constants.FragmentConstants;
+import com.liferay.fragment.constants.FragmentEntryLinkConstants;
 import com.liferay.fragment.model.FragmentEntry;
+import com.liferay.fragment.model.FragmentEntryLink;
+import com.liferay.fragment.renderer.DefaultFragmentRendererContext;
+import com.liferay.fragment.renderer.FragmentRendererController;
 import com.liferay.fragment.service.base.FragmentEntryServiceBaseImpl;
 import com.liferay.portal.aop.AopService;
 import com.liferay.portal.dao.orm.custom.sql.CustomSQL;
+import com.liferay.portal.events.ServicePreAction;
+import com.liferay.portal.events.ThemeServicePreAction;
 import com.liferay.portal.kernel.dao.orm.WildcardMode;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.LayoutSet;
+import com.liferay.portal.kernel.security.access.control.AccessControlUtil;
+import com.liferay.portal.kernel.security.auth.AccessControlContext;
 import com.liferay.portal.kernel.security.permission.ActionKeys;
 import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermission;
+import com.liferay.portal.kernel.service.CompanyLocalService;
+import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.service.LayoutLocalService;
 import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.InstancePool;
 import com.liferay.portal.kernel.util.OrderByComparator;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.taglib.util.ThemeUtil;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
 
 /**
  * @author Jürgen Kappler
@@ -380,6 +405,72 @@ public class FragmentEntryServiceImpl extends FragmentEntryServiceBaseImpl {
 	}
 
 	@Override
+	public String renderFragmentEntryPreview(
+			long groupId, String css, String html, String js)
+		throws Exception {
+
+		_portletResourcePermission.check(
+			getPermissionChecker(), groupId,
+			FragmentActionKeys.MANAGE_FRAGMENT_ENTRIES);
+
+		FragmentEntryLink fragmentEntryLink =
+			fragmentEntryLinkPersistence.create(0);
+
+		fragmentEntryLink.setCss(css);
+		fragmentEntryLink.setHtml(html);
+		fragmentEntryLink.setJs(js);
+
+		DefaultFragmentRendererContext defaultFragmentRendererContext =
+			new DefaultFragmentRendererContext(fragmentEntryLink);
+
+		defaultFragmentRendererContext.setMode(FragmentEntryLinkConstants.VIEW);
+
+		AccessControlContext accessControlContext =
+			AccessControlUtil.getAccessControlContext();
+
+		HttpServletRequest httpServletRequest =
+			accessControlContext.getRequest();
+
+		HttpServletResponse httpServletResponse =
+			accessControlContext.getResponse();
+
+		ServicePreAction servicePreAction = (ServicePreAction)InstancePool.get(
+			ServicePreAction.class.getName());
+
+		ThemeServicePreAction themeServicePreAction =
+			(ThemeServicePreAction)InstancePool.get(
+				ThemeServicePreAction.class.getName());
+
+		servicePreAction.run(httpServletRequest, httpServletResponse);
+		themeServicePreAction.run(httpServletRequest, httpServletResponse);
+
+		httpServletRequest.setAttribute(
+			WebKeys.CTX, httpServletRequest.getServletContext());
+
+		ThemeDisplay themeDisplay =
+			(ThemeDisplay)httpServletRequest.getAttribute(
+				WebKeys.THEME_DISPLAY);
+
+		LayoutSet layoutSet = themeDisplay.getLayoutSet();
+
+		String portalBody = ThemeUtil.include(
+			httpServletRequest.getServletContext(), httpServletRequest,
+			httpServletResponse, "portal_normal.ftl", layoutSet.getTheme(),
+			false);
+
+		Document document = Jsoup.parse(portalBody);
+
+		Element body = document.body();
+
+		body.html(
+			_fragmentRendererController.render(
+				defaultFragmentRendererContext, httpServletRequest,
+				accessControlContext.getResponse()));
+
+		return document.html();
+	}
+
+	@Override
 	public FragmentEntry updateFragmentEntry(
 			long fragmentEntryId, long previewFileEntryId)
 		throws PortalException {
@@ -446,7 +537,22 @@ public class FragmentEntryServiceImpl extends FragmentEntryServiceBaseImpl {
 	}
 
 	@Reference
+	private CompanyLocalService _companyLocalService;
+
+	@Reference
 	private CustomSQL _customSQL;
+
+	@Reference(cardinality = ReferenceCardinality.OPTIONAL)
+	private FragmentRendererController _fragmentRendererController;
+
+	@Reference
+	private GroupLocalService _groupLocalService;
+
+	@Reference
+	private LayoutLocalService _layoutLocalService;
+
+	@Reference
+	private Portal _portal;
 
 	@Reference(
 		target = "(resource.name=" + FragmentConstants.RESOURCE_NAME + ")"
