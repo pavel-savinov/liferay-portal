c4f84363c45aa52ed6afd770c64de085fecc5896
Pavel Savinov <pavel.savinov@liferay.com>
2019-06-05 11:36:48 +0200

    LPS-96435 Remote service to render a fragment preview which was not saved yet

--- modules/apps/fragment/fragment-service/src/main/java/com/liferay/fragment/service/impl/FragmentEntryServiceImpl.java
+++ modules/apps/fragment/fragment-service/src/main/java/com/liferay/fragment/service/impl/FragmentEntryServiceImpl.java
@@ -16,21 +16,48 @@ package com.liferay.fragment.service.impl;
 
 import com.liferay.fragment.constants.FragmentActionKeys;
 import com.liferay.fragment.constants.FragmentConstants;
+import com.liferay.fragment.constants.FragmentEntryLinkConstants;
 import com.liferay.fragment.model.FragmentEntry;
+import com.liferay.fragment.model.FragmentEntryLink;
+import com.liferay.fragment.renderer.DefaultFragmentRendererContext;
+import com.liferay.fragment.renderer.FragmentRendererController;
 import com.liferay.fragment.service.base.FragmentEntryServiceBaseImpl;
 import com.liferay.portal.aop.AopService;
 import com.liferay.portal.dao.orm.custom.sql.CustomSQL;
 import com.liferay.portal.kernel.dao.orm.WildcardMode;
 import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.model.Group;
+import com.liferay.portal.kernel.model.Layout;
+import com.liferay.portal.kernel.model.LayoutConstants;
+import com.liferay.portal.kernel.model.LayoutSet;
+import com.liferay.portal.kernel.model.LayoutTypePortlet;
+import com.liferay.portal.kernel.model.Theme;
+import com.liferay.portal.kernel.security.access.control.AccessControlUtil;
+import com.liferay.portal.kernel.security.auth.AccessControlContext;
 import com.liferay.portal.kernel.security.permission.ActionKeys;
 import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermission;
+import com.liferay.portal.kernel.service.CompanyLocalService;
+import com.liferay.portal.kernel.service.GroupLocalService;
+import com.liferay.portal.kernel.service.LayoutLocalService;
 import com.liferay.portal.kernel.service.ServiceContext;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.LocaleUtil;
 import com.liferay.portal.kernel.util.OrderByComparator;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.taglib.util.ThemeUtil;
 
 import java.util.List;
 
+import javax.servlet.http.HttpServletRequest;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
 import org.osgi.service.component.annotations.Component;
 import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.ReferenceCardinality;
 
 /**
  * @author Jürgen Kappler
@@ -380,6 +407,56 @@ public class FragmentEntryServiceImpl extends FragmentEntryServiceBaseImpl {
 	}
 
 	@Override
+	public String renderFragmentEntryPreview(
+			long groupId, String css, String html, String js)
+		throws Exception {
+
+		_portletResourcePermission.check(
+			getPermissionChecker(), groupId,
+			FragmentActionKeys.MANAGE_FRAGMENT_ENTRIES);
+
+		FragmentEntryLink fragmentEntryLink =
+			fragmentEntryLinkPersistence.create(0);
+
+		fragmentEntryLink.setCss(css);
+		fragmentEntryLink.setHtml(html);
+		fragmentEntryLink.setJs(js);
+
+		DefaultFragmentRendererContext defaultFragmentRendererContext =
+			new DefaultFragmentRendererContext(fragmentEntryLink);
+
+		defaultFragmentRendererContext.setMode(FragmentEntryLinkConstants.VIEW);
+
+		AccessControlContext accessControlContext =
+			AccessControlUtil.getAccessControlContext();
+
+		HttpServletRequest httpServletRequest =
+			accessControlContext.getRequest();
+
+		Group group = _groupLocalService.getGroup(groupId);
+
+		LayoutSet layoutSet = group.getPublicLayoutSet();
+
+		_initThemeDisplay(httpServletRequest, group);
+
+		String portalBody = ThemeUtil.include(
+			httpServletRequest.getServletContext(), httpServletRequest,
+			accessControlContext.getResponse(), "portal_normal.ftl",
+			layoutSet.getTheme(), false);
+
+		Document document = Jsoup.parse(portalBody);
+
+		Element body = document.body();
+
+		body.html(
+			_fragmentRendererController.render(
+				defaultFragmentRendererContext, httpServletRequest,
+				accessControlContext.getResponse()));
+
+		return document.html();
+	}
+
+	@Override
 	public FragmentEntry updateFragmentEntry(
 			long fragmentEntryId, long previewFileEntryId)
 		throws PortalException {
@@ -445,8 +522,76 @@ public class FragmentEntryServiceImpl extends FragmentEntryServiceBaseImpl {
 			previewFileEntryId, status);
 	}
 
+	private void _initThemeDisplay(
+			HttpServletRequest httpServletRequest, Group group)
+		throws Exception {
+
+		ThemeDisplay themeDisplay = new ThemeDisplay();
+
+		LayoutSet layoutSet = group.getPublicLayoutSet();
+
+		Theme theme = layoutSet.getTheme();
+
+		theme.setSetting("show_header", Boolean.FALSE.toString());
+
+		Layout layout = _layoutLocalService.getLayout(
+			_layoutLocalService.getDefaultPlid(group.getGroupId()));
+
+		themeDisplay.setCompany(
+			_companyLocalService.getCompany(group.getCompanyId()));
+		themeDisplay.setLanguageId(group.getDefaultLanguageId());
+		themeDisplay.setLayout(layout);
+
+		List<Layout> layouts = _layoutLocalService.getLayouts(
+			group.getGroupId(), false,
+			LayoutConstants.DEFAULT_PARENT_LAYOUT_ID);
+
+		themeDisplay.setLayouts(layouts);
+
+		themeDisplay.setLayoutSet(layoutSet);
+		themeDisplay.setLayoutTypePortlet(
+			(LayoutTypePortlet)layout.getLayoutType());
+		themeDisplay.setLocale(
+			LocaleUtil.fromLanguageId(themeDisplay.getLanguageId()));
+		themeDisplay.setPermissionChecker(getPermissionChecker());
+		themeDisplay.setRealUser(getUser());
+		themeDisplay.setRequest(httpServletRequest);
+		themeDisplay.setSecure(_portal.isForwardedSecure(httpServletRequest));
+		themeDisplay.setServerName(
+			_portal.getForwardedHost(httpServletRequest));
+		themeDisplay.setServerPort(
+			_portal.getForwardedPort(httpServletRequest));
+		themeDisplay.setSiteGroupId(group.getGroupId());
+		themeDisplay.setScopeGroupId(group.getGroupId());
+		themeDisplay.setUser(getUser());
+
+		themeDisplay.setLookAndFeel(theme, layoutSet.getColorScheme());
+
+		httpServletRequest.setAttribute(
+			WebKeys.CTX, httpServletRequest.getServletContext());
+		httpServletRequest.setAttribute(WebKeys.LAYOUT, layout);
+		httpServletRequest.setAttribute(
+			WebKeys.PORTLET_DECORATE, Boolean.FALSE);
+		httpServletRequest.setAttribute(WebKeys.THEME_DISPLAY, themeDisplay);
+	}
+
+	@Reference
+	private CompanyLocalService _companyLocalService;
+
 	@Reference
 	private CustomSQL _customSQL;
+
+	@Reference(cardinality = ReferenceCardinality.OPTIONAL)
+	private FragmentRendererController _fragmentRendererController;
+
+	@Reference
+	private GroupLocalService _groupLocalService;
+
+	@Reference
+	private LayoutLocalService _layoutLocalService;
+
+	@Reference
+	private Portal _portal;
 
 	@Reference(
 		target = "(resource.name=" + FragmentConstants.RESOURCE_NAME + ")"
