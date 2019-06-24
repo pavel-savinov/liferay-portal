16e2b177404b16291f154934c77b0e2c7eb64eab
Jürgen Kappler <jurgen.kappler@liferay.com>
2019-06-24 12:25:22 +0200

    LPS-96705 Display field helper for asset entry objects

---
+++ modules/apps/asset/asset-info-display-impl/src/main/java/com/liferay/asset/info/display/internal/field/AssetEntryInfoDisplayFieldHelperImpl.java
@@ -0,0 +1,74 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.asset.info.display.internal.field;
+
+import com.liferay.asset.info.display.field.AssetEntryInfoDisplayFieldHelper;
+import com.liferay.asset.kernel.AssetRendererFactoryRegistryUtil;
+import com.liferay.asset.kernel.model.AssetEntry;
+import com.liferay.asset.kernel.model.AssetRendererFactory;
+import com.liferay.info.display.contributor.field.InfoDisplayContributorField;
+import com.liferay.info.display.contributor.field.InfoDisplayContributorFieldTracker;
+import com.liferay.portal.kernel.exception.PortalException;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Locale;
+import java.util.Map;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author Jürgen Kappler
+ */
+@Component(immediate = true, service = AssetEntryInfoDisplayFieldHelper.class)
+public class AssetEntryInfoDisplayFieldHelperImpl
+	implements AssetEntryInfoDisplayFieldHelper {
+
+	public Map<String, Object> getAssetEntryInfoDisplayFieldsValues(
+			String className, long classPK, Locale locale)
+		throws PortalException {
+
+		Map<String, Object> infoDisplayFieldsValues = new HashMap<>();
+
+		AssetRendererFactory assetRendererFactory =
+			AssetRendererFactoryRegistryUtil.getAssetRendererFactoryByClassName(
+				className);
+
+		AssetEntry assetEntry = assetRendererFactory.getAssetEntry(
+			className, classPK);
+
+		List<InfoDisplayContributorField> infoDisplayContributorFields =
+			_infoDisplayContributorFieldTracker.getInfoDisplayContributorFields(
+				AssetEntry.class.getName());
+
+		for (InfoDisplayContributorField infoDisplayContributorField :
+				infoDisplayContributorFields) {
+
+			Object fieldValue = infoDisplayContributorField.getValue(
+				assetEntry, locale);
+
+			infoDisplayFieldsValues.putIfAbsent(
+				infoDisplayContributorField.getKey(), fieldValue);
+		}
+
+		return infoDisplayFieldsValues;
+	}
+
+	@Reference
+	private InfoDisplayContributorFieldTracker
+		_infoDisplayContributorFieldTracker;
+
+}
