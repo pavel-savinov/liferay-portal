6de4aaa5c22193c3f4bcdc1a685e327dffe2b1f5
Pavel Savinov <pavel.savinov@liferay.com>
2019-10-09 17:05:02 +0200

    LPS-102867 Don't remove other repeatable fields options

--- modules/apps/dynamic-data-mapping/dynamic-data-mapping-web/src/main/resources/META-INF/resources/js/ddm_form.js
+++ modules/apps/dynamic-data-mapping/dynamic-data-mapping-web/src/main/resources/META-INF/resources/js/ddm_form.js
@@ -876,14 +876,6 @@ AUI.add(
 				repeat() {
 					var instance = this;
 
-					var definition = instance.get('definition');
-
-					definition.fields.forEach(function(field) {
-						if (field.type === 'select') {
-							field.options.shift();
-						}
-					});
-
 					instance._getTemplate(function(fieldTemplate) {
 						var field = instance.createField(fieldTemplate);
 
