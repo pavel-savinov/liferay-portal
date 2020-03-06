a51bdc3e1bf5cc0fe243c2644c740ebc687ff957
Pavel Savinov <pavel.savinov@liferay.com>
2020-03-06 09:13:52 +0100

    LPS-109828 Use new finders to get a mix of fragment compositions and entries

--- modules/apps/fragment/fragment-service/src/main/java/com/liferay/fragment/service/impl/FragmentEntryServiceImpl.java
+++ modules/apps/fragment/fragment-service/src/main/java/com/liferay/fragment/service/impl/FragmentEntryServiceImpl.java
@@ -20,6 +20,7 @@ import com.liferay.fragment.model.FragmentEntry;
 import com.liferay.fragment.service.base.FragmentEntryServiceBaseImpl;
 import com.liferay.portal.aop.AopService;
 import com.liferay.portal.dao.orm.custom.sql.CustomSQL;
+import com.liferay.portal.kernel.dao.orm.QueryDefinition;
 import com.liferay.portal.kernel.dao.orm.WildcardMode;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.security.permission.resource.PortletResourcePermission;
@@ -146,6 +147,50 @@ public class FragmentEntryServiceImpl extends FragmentEntryServiceBaseImpl {
 		throws PortalException {
 
 		return fragmentEntryLocalService.fetchFragmentEntry(fragmentEntryId);
+	}
+
+	@Override
+	public List<Object> getFragmentCompositionsAndEntriesByNameAndStatus(
+		long groupId, long fragmentCollectionId, String name, int status,
+		int start, int end, OrderByComparator<?> orderByComparator) {
+
+		QueryDefinition<?> queryDefinition = new QueryDefinition<>(
+			status, start, end, (OrderByComparator<Object>)orderByComparator);
+
+		return fragmentEntryFinder.findFC_FE_ByG_FCI_LikeN(
+			groupId, fragmentCollectionId, name, queryDefinition);
+	}
+
+	@Override
+	public List<Object> getFragmentCompositionsAndEntriesByStatus(
+		long groupId, long fragmentCollectionId, int status, int start, int end,
+		OrderByComparator<?> orderByComparator) {
+
+		QueryDefinition<?> queryDefinition = new QueryDefinition<>(
+			status, start, end, (OrderByComparator<Object>)orderByComparator);
+
+		return fragmentEntryFinder.findFC_FE_ByG_FCI(
+			groupId, fragmentCollectionId, queryDefinition);
+	}
+
+	@Override
+	public int getFragmentCompositionsAndEntriesCountByNameAndStatus(
+		long groupId, long fragmentCollectionId, String name, int status) {
+
+		QueryDefinition<?> queryDefinition = new QueryDefinition<>(status);
+
+		return fragmentEntryFinder.countFC_FE_ByG_FCI_LikeN(
+			groupId, fragmentCollectionId, name, queryDefinition);
+	}
+
+	@Override
+	public int getFragmentCompositionsAndEntriesCountByStatus(
+		long groupId, long fragmentCollectionId, int status) {
+
+		QueryDefinition<?> queryDefinition = new QueryDefinition<>(status);
+
+		return fragmentEntryFinder.countFC_FE_ByG_FCI(
+			groupId, fragmentCollectionId, queryDefinition);
 	}
 
 	@Override
