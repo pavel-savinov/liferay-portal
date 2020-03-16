6f5ddbb9384ee6c2b99fcafab209c55144616414
Pavel Savinov <pavel.savinov@liferay.com>
2020-03-16 12:13:27 +0100

    LPS-109350 Use entity method instead of the ID one, we remove some dependencies there

--- modules/apps/layout/layout-page-template-service/src/main/java/com/liferay/layout/page/template/internal/importer/LayoutPageTemplatesImporterImpl.java
+++ modules/apps/layout/layout-page-template-service/src/main/java/com/liferay/layout/page/template/internal/importer/LayoutPageTemplatesImporterImpl.java
@@ -740,9 +740,7 @@ public class LayoutPageTemplatesImporterImpl
 
 		if (layoutPageTemplateStructure != null) {
 			_layoutPageTemplateStructureLocalService.
-				deleteLayoutPageTemplateStructure(
-					layoutPageTemplateStructure.
-						getLayoutPageTemplateStructureId());
+				deleteLayoutPageTemplateStructure(layoutPageTemplateStructure);
 		}
 
 		_layoutPageTemplateStructureLocalService.addLayoutPageTemplateStructure(
