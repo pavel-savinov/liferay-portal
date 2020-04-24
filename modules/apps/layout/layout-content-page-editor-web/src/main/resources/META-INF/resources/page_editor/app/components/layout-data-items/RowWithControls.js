9df5cd1ac1e809a67069e4c2e671ccb278fcbc51
Veronica Gonzalez <veronica.gonzalez@liferay.com>
2020-04-24 18:20:51 +0200

    LPS-102959 - Add vertical alingment for columns

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/layout-data-items/RowWithControls.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/layout-data-items/RowWithControls.js
@@ -27,6 +27,7 @@
  */
 
 import {useModal} from '@clayui/modal';
+import classNames from 'classnames';
 import {useIsMounted} from 'frontend-js-react-web';
 import PropTypes from 'prop-types';
 import React, {useEffect, useRef, useState} from 'react';
@@ -53,6 +54,7 @@ export const ResizingContext = React.createContext();
 
 const RowWithControls = React.forwardRef(
 	({children, item, layoutData}, ref) => {
+		const {config} = layoutData.items[item.itemId];
 		const dispatch = useDispatch();
 		const {gutters} = {
 			...LAYOUT_DATA_ITEM_DEFAULT_CONFIGURATIONS[
@@ -74,6 +76,9 @@ const RowWithControls = React.forwardRef(
 		});
 
 		const state = useSelector(state => state);
+		const {selectedViewportSize} = state;
+		const selectedViewportSizeConfig =
+			config[selectedViewportSize] || config;
 
 		const rowRef = useRef(null);
 		const rowRect = getRect(rowRef.current);
@@ -184,7 +189,14 @@ const RowWithControls = React.forwardRef(
 		return (
 			<Topper item={item} itemRef={ref} layoutData={layoutData}>
 				<Row
-					className="page-editor__row"
+					className={classNames('page-editor__row', {
+						'align-bottom':
+							selectedViewportSizeConfig.verticalAlignment ===
+							'bottom',
+						'align-middle':
+							selectedViewportSizeConfig.verticalAlignment ===
+							'middle',
+					})}
 					item={item}
 					layoutData={layoutData}
 					ref={node => {
