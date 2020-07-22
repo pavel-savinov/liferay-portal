d1ba1a5aa0a0048e5d87f8f0fec2fcebe8e73621
Sandro Chinea <sandrochinea@gmail.com>
2020-07-22 10:39:12 +0100

    WIP - Use empty object for values

--- modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/floating-toolbar/FragmentStylesPanel.js
+++ modules/apps/layout/layout-content-page-editor-web/src/main/resources/META-INF/resources/page_editor/app/components/floating-toolbar/FragmentStylesPanel.js
@@ -136,7 +136,7 @@ const CommonStyles = ({commonStyles, item, onValueSelect}) => (
 					key={index}
 					label={fieldSet.label}
 					onValueSelect={onValueSelect}
-					values={item.config.styles}
+					values={item.config.styles || {}}
 				/>
 			);
 		})}
