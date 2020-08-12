5ca7060b442ae9c32b64cca6ed2bc2f539ebf0d2
Jorge Ferrer <jorge.ferrer@liferay.com>
2020-08-12 17:31:41 +0200

    LPS-118979 Reorganize fields for Journal Article

--- modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/info/item/provider/JournalArticleInfoItemFormProvider.java
+++ modules/apps/journal/journal-web/src/main/java/com/liferay/journal/web/internal/info/item/provider/JournalArticleInfoItemFormProvider.java
@@ -25,17 +25,14 @@ import com.liferay.expando.info.item.provider.ExpandoInfoItemFieldSetProvider;
 import com.liferay.info.exception.NoSuchClassTypeException;
 import com.liferay.info.exception.NoSuchFormVariationException;
 import com.liferay.info.field.InfoFieldSet;
-import com.liferay.info.field.InfoFieldSetEntry;
 import com.liferay.info.form.InfoForm;
 import com.liferay.info.item.field.reader.InfoItemFieldReaderFieldSetProvider;
 import com.liferay.info.item.provider.InfoItemFormProvider;
+import com.liferay.info.localized.InfoLocalizedValue;
 import com.liferay.journal.model.JournalArticle;
 import com.liferay.journal.web.internal.info.item.JournalArticleInfoItemFields;
 import com.liferay.portal.kernel.exception.PortalException;
 import com.liferay.portal.kernel.util.GetterUtil;
-
-import java.util.Arrays;
-import java.util.Collection;
 
 import org.osgi.framework.Constants;
 import org.osgi.service.component.annotations.Component;
@@ -114,22 +111,59 @@ public class JournalArticleInfoItemFormProvider
 				GetterUtil.getLong(formVariationKey), groupId));
 	}
 
+	private InfoFieldSet _getBasicInformationInfoFieldSet() {
+		return InfoFieldSet.builder(
+		).infoFieldSetEntry(
+			JournalArticleInfoItemFields.titleInfoField
+		).infoFieldSetEntry(
+			JournalArticleInfoItemFields.descriptionInfoField
+		).infoFieldSetEntry(
+			JournalArticleInfoItemFields.publishDateInfoField
+		).infoFieldSetEntry(
+			JournalArticleInfoItemFields.authorNameInfoField
+		).infoFieldSetEntry(
+			JournalArticleInfoItemFields.authorProfileImageInfoField
+		).infoFieldSetEntry(
+			JournalArticleInfoItemFields.lastEditorNameInfoField
+		).infoFieldSetEntry(
+			JournalArticleInfoItemFields.lastEditorProfileImageInfoField
+		).labelInfoLocalizedValue(
+			InfoLocalizedValue.localize(getClass(), "basic-information")
+		).name(
+			"basic-information"
+		).build();
+	}
+
+	private InfoFieldSet _getDisplayPageInfoFieldSet() {
+		return InfoFieldSet.builder(
+		).infoFieldSetEntry(
+			JournalArticleInfoItemFields.displayPageUrlInfoField
+		).labelInfoLocalizedValue(
+			InfoLocalizedValue.localize(getClass(), "display-page")
+		).name(
+			"display-page"
+		).build();
+	}
+
+	private InfoFieldSet _getFeaturedImageInfoFieldSet() {
+		return InfoFieldSet.builder(
+		).infoFieldSetEntry(
+			JournalArticleInfoItemFields.smallImageInfoField
+		).labelInfoLocalizedValue(
+			InfoLocalizedValue.localize(getClass(), "featured-image")
+		).name(
+			"featured-image"
+		).build();
+	}
+
 	private InfoForm _getInfoForm(
 			long ddmStructureId, InfoFieldSet assetEntryInfoFieldSet)
 		throws NoSuchFormVariationException {
 
 		try {
 			return InfoForm.builder(
-			).infoFieldSetEntries(
-				_getJournalArticleFields()
 			).infoFieldSetEntry(
-				_infoItemFieldReaderFieldSetProvider.getInfoFieldSet(
-					JournalArticle.class.getName())
-			).infoFieldSetEntry(
-				assetEntryInfoFieldSet
-			).infoFieldSetEntry(
-				_expandoInfoItemFieldSetProvider.getInfoFieldSet(
-					JournalArticle.class.getName())
+				_getBasicInformationInfoFieldSet()
 			).<NoSuchStructureException>infoFieldSetEntry(
 				consumer -> {
 					if (ddmStructureId != 0) {
@@ -142,6 +176,18 @@ public class JournalArticleInfoItemFormProvider
 								getInfoItemFieldSet(ddmStructureId));
 					}
 				}
+			).infoFieldSetEntry(
+				_getDisplayPageInfoFieldSet()
+			).infoFieldSetEntry(
+				_getFeaturedImageInfoFieldSet()
+			).infoFieldSetEntry(
+				_expandoInfoItemFieldSetProvider.getInfoFieldSet(
+					JournalArticle.class.getName())
+			).infoFieldSetEntry(
+				assetEntryInfoFieldSet
+			).infoFieldSetEntry(
+				_infoItemFieldReaderFieldSetProvider.getInfoFieldSet(
+					JournalArticle.class.getName())
 			).name(
 				JournalArticle.class.getName()
 			).build();
@@ -150,19 +196,6 @@ public class JournalArticleInfoItemFormProvider
 			throw new NoSuchFormVariationException(
 				String.valueOf(ddmStructureId), noSuchStructureException);
 		}
-	}
-
-	private Collection<InfoFieldSetEntry> _getJournalArticleFields() {
-		return Arrays.asList(
-			JournalArticleInfoItemFields.titleInfoField,
-			JournalArticleInfoItemFields.descriptionInfoField,
-			JournalArticleInfoItemFields.smallImageInfoField,
-			JournalArticleInfoItemFields.authorNameInfoField,
-			JournalArticleInfoItemFields.authorProfileImageInfoField,
-			JournalArticleInfoItemFields.lastEditorNameInfoField,
-			JournalArticleInfoItemFields.lastEditorProfileImageInfoField,
-			JournalArticleInfoItemFields.publishDateInfoField,
-			JournalArticleInfoItemFields.displayPageUrlInfoField);
 	}
 
 	@Reference
