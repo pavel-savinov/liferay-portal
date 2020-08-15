7dfe6431cb6ac970d1c39c7be67c0838e1812cc0
Jürgen Kappler <jurgen.kappler@liferay.com>
2020-08-15 17:58:49 +0200

    LPS-118906 Replace usages of InfoDisplayWebKeys.INFO_DISPLAY_CONTRIBUTOR for LayoutDisplayPageWebKeys.LAYOUT_DISPLAY_PAGE_PROVIDER, since we are no longer injecting the attribute in the request

--- modules/apps/layout/layout-type-controller/layout-type-controller-portlet/src/main/java/com/liferay/layout/type/controller/portlet/internal/layout/type/controller/PortletLayoutTypeController.java
+++ modules/apps/layout/layout-type-controller/layout-type-controller-portlet/src/main/java/com/liferay/layout/type/controller/portlet/internal/layout/type/controller/PortletLayoutTypeController.java
@@ -16,9 +16,9 @@ package com.liferay.layout.type.controller.portlet.internal.layout.type.controll
 
 import com.liferay.fragment.constants.FragmentActionKeys;
 import com.liferay.fragment.renderer.FragmentRendererController;
-import com.liferay.info.display.contributor.InfoDisplayContributorTracker;
 import com.liferay.info.item.InfoItemServiceTracker;
 import com.liferay.info.list.renderer.InfoListRendererTracker;
+import com.liferay.layout.display.page.LayoutDisplayPageProviderTracker;
 import com.liferay.layout.list.retriever.LayoutListRetrieverTracker;
 import com.liferay.layout.list.retriever.ListObjectReferenceFactoryTracker;
 import com.liferay.layout.type.controller.BaseLayoutTypeControllerImpl;
@@ -98,8 +98,8 @@ public class PortletLayoutTypeController extends BaseLayoutTypeControllerImpl {
 			PortletLayoutDisplayContext.class.getName(),
 			new PortletLayoutDisplayContext(
 				httpServletRequest, httpServletResponse,
-				_infoDisplayContributorTracker, _infoItemServiceTracker,
-				_infoListRendererTracker, _layoutListRetrieverTracker,
+				_infoItemServiceTracker, _infoListRendererTracker,
+				_layoutDisplayPageProviderTracker, _layoutListRetrieverTracker,
 				_listObjectReferenceFactoryTracker));
 
 		RequestDispatcher requestDispatcher =
@@ -226,13 +226,13 @@ public class PortletLayoutTypeController extends BaseLayoutTypeControllerImpl {
 	private FragmentRendererController _fragmentRendererController;
 
 	@Reference
-	private InfoDisplayContributorTracker _infoDisplayContributorTracker;
-
-	@Reference
 	private InfoItemServiceTracker _infoItemServiceTracker;
 
 	@Reference
 	private InfoListRendererTracker _infoListRendererTracker;
+
+	@Reference
+	private LayoutDisplayPageProviderTracker _layoutDisplayPageProviderTracker;
 
 	@Reference
 	private LayoutListRetrieverTracker _layoutListRetrieverTracker;
